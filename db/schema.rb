7c02b147165e6abc7de05dec0cfe3dfbcce7603f
Seong-June Kim <seong-june.kim@yale.edu>
2016-05-23 14:14:24 -0400

    Using active_record_store for session store.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160519142122) do
+ActiveRecord::Schema.define(version: 20160523181121) do
 
   create_table "admin_manifests", force: :cascade do |t|
     t.integer  "room_id",    limit: 4
@@ -55,6 +55,16 @@ ActiveRecord::Schema.define(version: 20160519142122) do
     t.datetime "created_at",             null: false
     t.datetime "updated_at",             null: false
   end
+
+  create_table "sessions", force: :cascade do |t|
+    t.string   "session_id", limit: 255,   null: false
+    t.text     "data",       limit: 65535
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "sessions", ["session_id"], name: "index_sessions_on_session_id", unique: true, using: :btree
+  add_index "sessions", ["updated_at"], name: "index_sessions_on_updated_at", using: :btree
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  limit: 255, default: "", null: false
