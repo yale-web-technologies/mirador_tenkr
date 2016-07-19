e74dcbda2ae9da05340990fc6552ea23c03d4622
Seong-June Kim <seong-june.kim@yale.edu>
2016-07-19 15:25:22 -0400

    Print version of Yale Mirador separately.

--- app/helpers/admin/home_helper.rb
+++ app/helpers/admin/home_helper.rb
@@ -1,15 +1,26 @@
 module Admin::HomeHelper
   def version_info
-    mirador_head = ''
+    mirador_version = ''
+    yale_mirador_version = ''
+    
     File.open("#{Rails.root}/public/mirador/mirador.js") do |f|
-      mirador_head << f.readline
-      mirador_head << '; '
-      mirador_head << f.readline
+      mirador_version << f.readline
+      mirador_version << '; '
+      mirador_version << f.readline
     end
-    mirador_head.gsub!('//!', '')
+    File.open("#{Rails.root}/public/yale-mirador/version.json") do |f|
+      obj = JSON.parse(f.read)
+      yale_mirador_version << obj['version']
+      yale_mirador_version << '; '
+      yale_mirador_version << obj['buildDate']
+    end
+    
+    mirador_version.gsub!('//!', '')
+    
     [
-      "<p><b>Version:</b> #{Miradorails::VERSION}</p>",
-      "<p><b>Mirador:</b> #{mirador_head}</p>"
+      "<p><b>Mirador Core:</b> #{mirador_version}</p>",
+      "<p><b>Yale Mirador:</b> #{yale_mirador_version}</p>",
+      "<p><b>Mirador/Rails Version:</b> #{Miradorails::VERSION}</p>",
     ].join("\n")
   end
 end
