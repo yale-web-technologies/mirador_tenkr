506e2d1d88bed7f9011d9b7eeb46355c91658566
Seong-June Kim <seong-june.kim@yale.edu>
2016-08-23 14:11:04 -0400

    Include css of Semantic UI after Mirador core in order not to let Bootstrap included in Mirador core override Semantic UI settings.

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -9,4 +9,4 @@ Rails.application.config.assets.version = '1.0'
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 
-Rails.application.config.assets.precompile += %w( jquery.js goldenlayout-base.css goldenlayout.min.js )
+Rails.application.config.assets.precompile += %w( semantic_ui/semantic_ui.css jquery.js goldenlayout-base.css goldenlayout.min.js )
