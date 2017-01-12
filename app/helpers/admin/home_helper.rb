bcffdf3299a7cd736fd38f34095728bd3c25a945
Seong-June Kim <seong-june.kim@yale.edu>
2017-01-12 16:42:29 -0500

    Hide version info
    Since Mirador core stopped prepending version info to the source, and Yale-Mirador is in the middle of changing the way version info is specified.

--- app/helpers/admin/home_helper.rb
+++ app/helpers/admin/home_helper.rb
@@ -3,17 +3,17 @@ module Admin::HomeHelper
     mirador_version = ''
     yale_mirador_version = ''
     
-    File.open("#{Rails.root}/public/mirador/mirador.js") do |f|
-      mirador_version << f.readline
-      mirador_version << '; '
-      mirador_version << f.readline
-    end
-    File.open("#{Rails.root}/public/yale-mirador/version.json") do |f|
-      obj = JSON.parse(f.read)
-      yale_mirador_version << obj['version']
-      yale_mirador_version << '; '
-      yale_mirador_version << obj['buildDate']
-    end
+    # File.open("#{Rails.root}/public/mirador/mirador.min.js") do |f|
+    #   mirador_version << f.readline
+    #   mirador_version << '; '
+    #   mirador_version << f.readline
+    # end
+    # File.open("#{Rails.root}/public/yale-mirador/version.json") do |f|
+    #   obj = JSON.parse(f.read)
+    #   yale_mirador_version << obj['version']
+    #   yale_mirador_version << '; '
+    #   yale_mirador_version << obj['buildDate']
+    # end
     
     mirador_version.gsub!('//!', '')
     
