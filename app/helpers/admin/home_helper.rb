456c7ca0b52ccb38d5a467d6465841ce575dfd9e
Seong-June Kim <seong-june.kim@yale.edu>
2017-03-01 09:48:00 -0500

    Version strings

--- app/helpers/admin/home_helper.rb
+++ app/helpers/admin/home_helper.rb
@@ -2,25 +2,24 @@ module Admin::HomeHelper
   def version_info
     mirador_version = ''
     yale_mirador_version = ''
-    
-    # File.open("#{Rails.root}/public/mirador/mirador.min.js") do |f|
-    #   mirador_version << f.readline
-    #   mirador_version << '; '
-    #   mirador_version << f.readline
-    # end
-    # File.open("#{Rails.root}/public/yale-mirador/version.json") do |f|
-    #   obj = JSON.parse(f.read)
-    #   yale_mirador_version << obj['version']
-    #   yale_mirador_version << '; '
-    #   yale_mirador_version << obj['buildDate']
-    # end
-    
+
+    File.open("#{Rails.root}/vendor/assets/mirador/mirador.min.js") do |f|
+      mirador_version << f.readline
+      mirador_version << '; '
+      mirador_version << f.readline
+    end
+
+    File.open("#{Rails.root}/vendor/assets/yale-mirador/yale-mirador.bundle.js") do |f|
+      ym_version = f.readline.sub(/^\/\/.s*/, '')
+      yale_mirador_version << ym_version
+    end
+
     mirador_version.gsub!('//!', '')
-    
+
     [
       "<p><b>Mirador Core:</b> #{mirador_version}</p>",
       "<p><b>Yale Mirador:</b> #{yale_mirador_version}</p>",
-      "<p><b>Mirador/Rails Version:</b> #{Miradorails::VERSION}</p>",
+      "<p><b>Mirador/Rails:</b> #{Miradorails::VERSION}</p>",
     ].join("\n")
   end
 end
--- config/initializers/version.rb
+++ config/initializers/version.rb
@@ -1,3 +1,3 @@
 module Miradorails
-  VERSION = '0.9.0'
+  VERSION = '0.10.0'
 end
