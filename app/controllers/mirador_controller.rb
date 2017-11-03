13ae333159966034701b618c08a4294ba33c95f0
Seong-June Kim <seong-june.kim@yale.edu>
2017-11-02 22:45:01 -0400

    New way to pass parameters to mirador.
    Everything is now passed as parameters to App.init().

--- app/controllers/mirador_controller.rb
+++ app/controllers/mirador_controller.rb
@@ -3,16 +3,23 @@ class MiradorController < ApplicationController
     @is_mirador_window = true
     manifest_id = params[:manifest]
     manifest = Admin::Manifest.find(manifest_id)
-    @manifest_url = manifest.url
-    @title = "Mirador - #{manifest.title}"
-    @room_id = params[:room_id]
-    @canvas_id = params[:canvas] || ''
-    @toc_tags = params[:tocTags] || ''
-    @layer_ids = params[:layers] || ''
-    @annotation_id = params[:annotation] || ''
+    @title = "Viewer - #{manifest.title}"
+    canvas_id = params[:canvas] || ''
+    annotation_id = params[:annotation] || ''
 
-    db_setting = Admin::Setting.first
-    @settings = parse_json(db_setting.site_mirador_settings)
+    db_settings = Admin::Setting.first
+    @settings = parse_json(db_settings.site_mirador_settings)
+
+    window = {
+      type: 'imageWindow',
+      manifest: manifest.url
+    }
+    window[:canvas] = canvas_id unless canvas_id.empty?
+    window[:annotation] = annotation_id unless annotation_id.empty?
+
+    @settings['initialLayout'] = {
+      windows: [ window ]
+    }
   end
 
 private
